fd722b5ab6243a12de4cc803dd1e284b8b932486
Ben Rowe <65142175+benrowe-chuffed@users.noreply.github.com>
2020-08-22 07:13:36 +1000

    Display diff output for pipeline logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-if [[ -n "$(terraform fmt -check -recursive)" ]]; then
+if [[ -n "$(terraform fmt -check -recursive -diff)" ]]; then
   echo "Some terraform files need be formatted, run 'terraform fmt' to fix";
   exit 1;
 fi
