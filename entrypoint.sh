80ec37fd0bdde1e3df8d8b26fa51ffa70ba4da7d
Jason Field <jason@avon-lea.co.uk>
2020-11-21 12:57:02 +0000

    Warning for action runs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
+echo "::warning ::Github Action: actionshub/terraform-lint has migrated to the main branch as default, the master branch will be removed"
 if [[ -n "$(terraform fmt -check -recursive -diff)" ]]; then
   echo "Some terraform files need be formatted, run 'terraform fmt' to fix";
   exit 1;
