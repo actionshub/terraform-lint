573f643bb2eb247266e5b6a5208a1d672d63d7c8
Abel Alejandro Nieva <abel@abelnieva.ar>
2020-12-03 12:38:27 +0100

    verbose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 if [[ -n "$(terraform fmt -check -recursive -diff)" ]]; then
+  terraform fmt -check -recursive -diff
+  terraform --version
   echo "Some terraform files need be formatted, run 'terraform fmt' to fix";
   exit 1;
 fi
