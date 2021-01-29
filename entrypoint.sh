668ae3f03b2bded5a1169b4554019ef384be4335
Abel Nieva <abelnieva@cloud.upwork.com>
2021-01-29 16:02:58 +0100

    running different way to capture error exit code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 echo "::warning ::Github Action: actionshub/terraform-lint has migrated to the main branch as default, the master branch will be removed"
-if [[ -n "$(terraform fmt -check -recursive -diff)" ]]; then
+terraform fmt -check -recursive -diff
+if [ $? -ne 0 ]; then
   echo "Some terraform files need be formatted, run 'terraform fmt' to fix";
   exit 1;
 fi
