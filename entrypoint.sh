7a0dd4eb49cf3c3017db374162f8fc6e7239891b
Dominik Wißler <31248794+Wysselbie@users.noreply.github.com>
2022-04-15 03:36:54 +0200

    fix: Added -recursive option to command hint (#13)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 if [[ -n "$(terraform fmt -check -recursive -diff)" ]]; then
-  echo "Some terraform files need be formatted, run 'terraform fmt' to fix";
+  echo "Some terraform files need to be formatted. Run 'terraform fmt -recursive' to fix them.";
   exit 1;
 fi
